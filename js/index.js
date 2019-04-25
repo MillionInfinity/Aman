b57ed9e4e367ef9043f1e059e3a8d56f6c3ea178
Million Yemane <wedi_ngus@yahoo.com>
2019-04-25 12:06:32 -0500

    without navbar

--- js/index.js
+++ js/index.js
@@ -21,42 +21,8 @@ console.log("ready!");
 const newNav =()=>{
     console.log("new nav");
     let theNav = `
-<header id="my-header">
-        <div class="navbar navbar-expand-lg navbar-light bg-light">
-
-         <a class="navbar-brand orn" href="#"><img src="./../imgs/logoflower2.png" width="38" height="35" class="d-inline-block align-top" alt="">Ornella Multi-Media</a>     
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-        <ul class="collapse navbar-collapse navfont" id="navbarText">
-                    <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active"><a class="nav-link n1" href="index.html">Home <span class="sr-only">(current)</span></a></li>
-                            <li class="nav-item"><a class="nav-link n2" href="aman.html">Aman</a></li>
-                            <li class="nav-item"><a class="nav-link n3" href="gallery.html">Gallery</a></li>
-                            <li class="nav-item"><a class="nav-link n4" href="http://localhost/ornella/OrnellaMedia/contactme.php">Contact Us</a></li>
-                            <li class="nav-item"><a class="nav-link n4" href="branch.html">Branch</a></li>
-                    </ul>
-                     <div class="central-nav">
-                        <a href="index.html"><img src="./../imgs/logoflower2.png" class="img-fluid" alt="ornella logo" width="150px"></a>
-                        <div class="orn">Ornella Multi-Media</div>
-                        <div class="creative">Creative Photographer and Videographer</div>
-                   </div> 
-                    <div class="navbar navbar-expand-sm bg-light justify-content-center"></div>
-            <span class="navbar-text m-2">
-                <div class="right-nav"> 
-                <div class="legend">Habesha Photographer & Videographer</div>
-                <div class="line"></div> 
-                <div class="follow"> follow me on social media </div>
-                <div class="icons">
-                <a href="https://twitter.com/amanzerumesfin"></a>
-                <a href="https://www.instagram.com/aman_zeru/"></a> 
-                <a href="https://www.facebook.com/begieZgreat/"></a>
-                </div>
-                </div>
-            </span>
-    </ul>
-        </div>
-
+        <header id="my-header">
+        
         </header>
 `;
     $("#godaddy").html(theNav);
@@ -66,16 +32,13 @@ newNav();
 
 const footer = () => {
     let footerh = `<div class="nav footer">
-               <div class=""></div>
-                 <div class="fi">
-                <img src="./../imgs/logoflower2.png" alt="textlogo" width="150px">
-                </div>
-               <div class="footer2 text-center">
-                <div class="f f1"><a href="#">Home</a><span>|</span></div>
-                <div class="f"><a href="#">Aman</a><span>|</span></div>
-                <div class="f"><a href="#">Gallery</a><span>|</span></div>
-                <div class="f"><a href="#">Branches</a></div>
-                </div> 
+                <div class="fi"><img src="./../imgs/logoflower2.png" alt="textlogo" width="150px"></div>
+                    <div class="footer2 text-center">
+                        <div class="f f1"><a href="#">Home</a><span>|</span></div>
+                        <div class="f"><a href="#">Aman</a><span>|</span></div>
+                        <div class="f"><a href="#">Gallery</a><span>|</span></div>
+                        <div class="f"><a href="#">Branches</a></div>
+                    </div> 
                 </div>`;
     $("#footer").html(footerh);
 };
