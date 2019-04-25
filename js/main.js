c8d475ab347a3af14b73570d3bb25555e14d99be
Million Yemane <wedi_ngus@yahoo.com>
2019-04-24 22:13:36 -0500

    copied files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 console.log("biet mekeyer");
-let page=require('./index');
+let page =require('./index');
+let  fetching=require('./fetch');
+
+// console.log("etl", fetching.loadOrnella);
