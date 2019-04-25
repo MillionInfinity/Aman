e792bdf07b15df95d9c606d37e6e40819e33d67e
Million Yemane <wedi_ngus@yahoo.com>
2019-04-24 19:20:12 -0500

    created more files

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 "use strict";
 console.log("ready!");
+
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 10) {
+//             $(".black").css("background", "blue");
+//         }
+
+//         else {
+//             $(".black").css("background", "#333");
+//         }
+//     })
+// })
