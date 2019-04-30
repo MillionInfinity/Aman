90126d16ce955cb08fbfc733c52bf76f8e5136b5
Million Yemane <wedi_ngus@yahoo.com>
2019-04-30 10:39:14 -0500

    node modules deleted

--- js/footer.js
+++ js/footer.js
@@ -1,54 +1,44 @@
 "use strict";
 const footer = () => {
     let footerh = `
-<!-- Footer -->
+
 <footer class="page-footer font-small special-color-dark pt-4">
 
-    <!-- Footer Elements -->
+   
     <div class="container">
-
-      <!-- Social buttons -->
-      <ul class="list-unstyled list-inline text-center">
-        <li class="list-inline-item">
-          <a class="btn-floating btn-fb mx-1">Home
-            <i class="fab fa-facebook-f"> </i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-tw mx-1">About
-            <i class="fab fa-twitter"> </i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-gplus mx-1">Gallery
-            <i class="fab fa-google-plus-g"> </i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-li mx-1">Branches
-            <i class="fab fa-linkedin-in"> </i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-dribbble mx-1">Countact me
-            <i class="fab fa-dribbble"> </i>
-          </a>
-        </li>
-      </ul>
-      <!-- Social buttons -->
-
-    </div>
-    <!-- Footer Elements -->
-
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+          <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-fb mx-1">Home
+                    <i class="fab fa-facebook-f"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-tw mx-1">About
+                    <i class="fab fa-twitter"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-gplus mx-1">Gallery
+                    <i class="fab fa-google-plus-g"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-li mx-1">Branches
+                    <i class="fab fa-linkedin-in"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-dribbble mx-1">Countact me
+                    <i class="fab fa-dribbble"> </i>
+                  </a>
+                </li>
+          </ul>
+     </div>
+       <div class="footer-copyright text-center py-3">© 2018 Copyright:
       <a href="https://millioninfinity.github.io/Personal-Site/"> Million Asseghegn</a>
     </div>
-    <!-- Copyright -->
-
-  </footer>
-  <!-- Footer -->
-`;
+     </footer>
+ `;
     $("#footers").html(footerh);
 };
 
@@ -60,7 +50,7 @@ const lowerpart = () => {
     <div class="d text-center">
     <p class="h">RECORDING <span>authentic</span>AND <span>intimate </span> TRADITIONAL WEDDINGS AS THEY ARE IN HOME</p>
     </div>
-    <div class="e"><img src="./imgs/logodesign.png" alt="textlogo" width="250px"></div>
+    <div class="e"><img src="./imgs/logodesign.png" alt="textlogo" width="250px"/></div>
 
     <div class="a tinone"><a href="aman.html"><div class="meet"><div class="met"></div></div><p class="textAman">Meet Aman</p></a></div>
     <div class="b tinone"><a href="#!"><div class="gal"><div class="met"></div></div><p class="textAman">Gallery</p></a></div>
@@ -71,37 +61,3 @@ const lowerpart = () => {
   $(".threeIn1").html(thepart);
 };
 lowerpart();
-
-
-
-// const lowerbranch = () => {
-//     // console.log("lower part of the page");
-//     let thepart = `
-//     <div class="d text-center">
-//     <p class="h">RECORDING <span>authentic</span>AND <span>intimate </span> TRADITIONAL WEDDINGS AS THEY ARE IN HOME</p>
-//     </div>
-//     <div class="e"><img src="./../imgs/logodesign.png" alt="textlogo" width="250px"></div>
-//      `;
-//     $(".threeIn2").html(thepart);
-// };
-// lowerbranch();
-
-// $('.form-control').on('click', function (e) {
-//     e.preventDefault();
-//     $(this).css('border', '2px solid #b9b1a7');
-// });
-
-// $('.submit').on('click', function (e) {
-//     e.preventDefault();
-//     $(this).css('background-color', '#ece5e7');
-//     $(this).css('color', 'rgb(138, 135, 135)');
-// });
-
-
-// module.exports = {
-
-//     lowerpart,
-//     lowerbranch
-
-
-// };
